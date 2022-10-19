d486626119e7d6ff28fdc0759a326bb5090a692d
Ludovic Muller <ludovicmuller1@gmail.com>
2022-10-19 19:38:12 +0200

    feat: create library in a dedicated file
    BREAKING CHANGE: the `sayHello` function should be imported from `lib.js`

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Say hello to someone in the console.
- *
- * @param {string} name Name of the person to great.
- */
-export const sayHello = (name = "world") => {
-  console.log(`Hello ${name}!`);
-};
+import { sayHello } from "./lib.js";
 
 sayHello();
---
+++ lib.js
@@ -0,0 +1,8 @@
+/**
+ * Say hello to someone in the console.
+ *
+ * @param {string} name Name of the person to great.
+ */
+export const sayHello = (name = "world") => {
+  console.log(`Hello ${name}!`);
+};
